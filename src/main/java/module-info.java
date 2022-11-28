51f6299c5e0097d6501eeb5a89ec765b25630b0b
Silas Lund <aeromaax22@gmail.com>
2022-11-28 16:34:21 +0100

    sql libraries and database connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module project.mytunes {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.xml.bind;
+    requires java.sql;
+    requires java.naming;
+    requires com.microsoft.sqlserver.jdbc;
 
     opens project.mytunes to javafx.fxml;
     exports project.mytunes;
